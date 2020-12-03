aa5f532f5e09d53fec0d9975523ce0fc85114943
Joseph Clough <joseph.clough@thedistance.co.uk>
2020-12-03 15:26:36 +0000

    fix: deployment fixes

--- apps/app/src/onboarding/onboarding.module.ts
+++ apps/app/src/onboarding/onboarding.module.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@lib/common/common.module';
 import { OnboardingModule } from '@lib/power/onboarding/onboarding.module';
 import { Module } from '@nestjs/common';
 import { OnboardingResolver } from './onboarding.resolver';
 
 @Module({
-  imports: [OnboardingModule],
+  imports: [OnboardingModule, CommonModule],
   providers: [OnboardingResolver],
 })
 export class OnboardingAppModule {}
