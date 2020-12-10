66e112fb1644346270ab04172445dcfb87096ca5
andy turner <andy.turner@thedistance.co.uk>
2020-12-10 14:58:58 +0000

    feat: implemented CMS allCountries. PDL-96

--- apps/cms/src/country/country.resolver.ts
+++ apps/cms/src/country/country.resolver.ts
@@ -1,5 +1,5 @@
 import { Args, Query, Resolver } from '@nestjs/graphql';
-import { CountryService } from '@lib/power/country';
+import { CountryFilter, CountryService } from '@lib/power/country';
 
 @Resolver('Country')
 export class CountryResolver {
@@ -8,5 +8,16 @@ export class CountryResolver {
   @Query('Country')
   async Country(@Args('id') id) {
     return await this.service.findById(id);
+  }
+
+  @Query('allCountries')
+  async allCountries(
+    @Args('page') page = 0,
+    @Args('perPage') perPage = 25,
+    @Args('sortField') sortField = 'country',
+    @Args('sortOrder') sortOrder: 'ASC' | 'DESC' = 'ASC',
+    @Args('filter') filter: CountryFilter = {},
+  ) {
+    return this.service.findAll(page, perPage, sortField, sortOrder, filter);
   }
 }
