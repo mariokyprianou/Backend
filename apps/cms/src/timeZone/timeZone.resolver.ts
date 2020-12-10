0bc53270cc4d315575f11fd0a25d2e52ec811a07
andy turner <andy.turner@thedistance.co.uk>
2020-12-10 14:51:11 +0000

    feat: implemented CMS allTimeZones. PDL-123

--- apps/cms/src/timeZone/timeZone.resolver.ts
+++ apps/cms/src/timeZone/timeZone.resolver.ts
@@ -1,5 +1,5 @@
 import { Args, Query, Resolver } from '@nestjs/graphql';
-import { TimeZoneService } from '@lib/power/timeZone';
+import { TimeZoneFilter, TimeZoneService } from '@lib/power/timeZone';
 
 @Resolver('TimeZone')
 export class TimeZoneResolver {
@@ -8,5 +8,16 @@ export class TimeZoneResolver {
   @Query('TimeZone')
   async TimeZone(@Args('id') id) {
     return await this.service.findById(id);
+  }
+
+  @Query('allTimeZones')
+  async allTimeZones(
+    @Args('page') page = 0,
+    @Args('perPage') perPage = 25,
+    @Args('sortField') sortField = 'time_zone',
+    @Args('sortOrder') sortOrder: 'ASC' | 'DESC' = 'ASC',
+    @Args('filter') filter: TimeZoneFilter = {},
+  ) {
+    return this.service.findAll(page, perPage, sortField, sortOrder, filter);
   }
 }
