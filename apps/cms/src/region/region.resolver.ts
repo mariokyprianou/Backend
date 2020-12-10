2fb90dd7baa2838abb7704477284f2877ae95622
andy turner <andy.turner@thedistance.co.uk>
2020-12-10 15:09:00 +0000

    feat: implemented CMS allRegions. PDL-99

--- apps/cms/src/region/region.resolver.ts
+++ apps/cms/src/region/region.resolver.ts
@@ -1,5 +1,5 @@
 import { Args, Query, Resolver } from '@nestjs/graphql';
-import { RegionService } from '@lib/power/region';
+import { RegionFilter, RegionService } from '@lib/power/region';
 
 @Resolver('Region')
 export class RegionResolver {
@@ -8,5 +8,16 @@ export class RegionResolver {
   @Query('Region')
   async Region(@Args('id') id) {
     return await this.service.findById(id);
+  }
+
+  @Query('allRegions')
+  async allRegions(
+    @Args('page') page = 0,
+    @Args('perPage') perPage = 25,
+    @Args('sortField') sortField = 'region',
+    @Args('sortOrder') sortOrder: 'ASC' | 'DESC' = 'ASC',
+    @Args('filter') filter: RegionFilter = {},
+  ) {
+    return this.service.findAll(page, perPage, sortField, sortOrder, filter);
   }
 }
