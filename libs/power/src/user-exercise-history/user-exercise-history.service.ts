abf5d88f33c840be90e7ea51b0a48e88ea882b37
Matt Jones <matt.jones@thedistance.co.uk>
2021-06-16 11:55:16 +0000

    fix(PDL-1135): share weight history for exercises with same category

--- libs/power/src/user-exercise-history/user-exercise-history.service.ts
+++ libs/power/src/user-exercise-history/user-exercise-history.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Exercise } from '../exercise';
 import { UserExerciseHistory } from './user-exercise-history.model';
 
 @Injectable()
@@ -7,9 +8,23 @@ export class UserExerciseHistoryService {
     accountId: string;
     exerciseId: string;
   }) {
+    // Include exercises that share the same category
+    const exercisesWithSameCategory = Exercise.query()
+      .select('category_exercises.id')
+      .where('exercise.id', params.exerciseId)
+      .join(
+        'exercise as category_exercises',
+        'exercise.category_id',
+        'category_exercises.category_id',
+      );
+
     return UserExerciseHistory.query()
-      .where('exercise_id', params.exerciseId)
-      .andWhere('account_id', params.accountId)
+      .where('account_id', params.accountId)
+      .where((qb) =>
+        qb
+          .where('exercise_id', params.exerciseId)
+          .orWhereIn('exercise_id', exercisesWithSameCategory),
+      )
       .orderBy('created_at', 'DESC')
       .orderBy('exercise_id')
       .orderBy('set_number', 'DESC');
